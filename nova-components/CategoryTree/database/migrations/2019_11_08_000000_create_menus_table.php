7ebe019a33a30b2a0e280927c809646c6024d349
rufat.gadirov <rufat@bafood.tech>
2022-06-30 19:06:39 +0400

    removed unused migrations

--- nova-components/CategoryTree/database/migrations/2019_11_08_000000_create_menus_table.php
+++ nova-components/CategoryTree/database/migrations/2019_11_08_000000_create_menus_table.php
@@ -15,28 +15,10 @@ return new class extends Migration
             $table->string('slug');
             $table->timestamps();
         });
-
-        Schema::create(MenuBuilder::getMenuItemsTableName(), function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('menu_id')->nullable();
-            $table->string('name');
-            $table->string('locale');
-            $table->string('class')->nullable();
-            $table->string('value')->nullable();
-            $table->string('target')->default('_self');
-            $table->json('data')->nullable();
-            $table->integer('parent_id')->nullable();
-            $table->integer('order');
-            $table->boolean('enabled')->default(1);
-            $table->timestamps();
-
-            $table->foreign('menu_id')->references('id')->on(MenuBuilder::getMenusTableName())->onDelete('cascade');
-        });
     }
 
     public function down()
     {
         Schema::dropIfExists(MenuBuilder::getMenuItemsTableName());
-        Schema::dropIfExists(MenuBuilder::getMenusTableName());
     }
 };
